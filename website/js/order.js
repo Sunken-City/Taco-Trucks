2c2203407edfab46ef191a922a1d2b78780201b9
picoriley <acloudy@mail.smu.edu>
2014-02-27 20:40:52 -0600

    Pushing some beans, as well as starting to style the orders page.

--- website/js/order.js
+++ website/js/order.js
@@ -2,9 +2,9 @@
 window.addEventListener('load', function(event) {
   
   //Set up the menu
-  $("#accordion").accordion({ active: 2 });
-  $("#time").spinner({
-    min : 0,
+  $("#accordion").accordion({ active: 0 });
+  $(".quantity").spinner({
+    min : 1,
     max : 100,
     showOn : 'both'
   });
