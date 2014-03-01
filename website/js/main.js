b24aa939dde39f51cb56b6141b2e24bdcecbf5ca
Ellyonia <ckeith@smu.edu>
2014-03-01 12:04:29 -0600

    Added an on click event to push the home page Create a taco button to forward to the order page

---
+++ website/js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(event) {
+	var createATacoButton = $('#createATaco');
+	console.log(createATacoButton);
+	createATacoButton.click(function(){
+	window.location.replace("order.html");
+
+
+	});
+
+});
