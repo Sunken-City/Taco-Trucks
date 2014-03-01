e4f659d4658bf0060fbcca7673320bd4f8e01af3
picoriley <acloudy@mail.smu.edu>
2014-03-01 17:15:04 -0600

    You can now select items on the menu!

--- website/js/order.js
+++ website/js/order.js
@@ -21,11 +21,11 @@ window.addEventListener('load', function(event) {
     for(var i = 0; i < json.menu.rice.length; i++){
       createMenu("rice", json.menu.rice[i].name);
     }
-    for(var i = 0; i < json.menu.cheese.length; i++){
-      createMenu("cheese", json.menu.cheese[i].name);
-    }
     for(var i = 0; i < json.menu.beans.length; i++){
       createMenu("bean", json.menu.beans[i].name);
+    }
+    for(var i = 0; i < json.menu.cheese.length; i++){
+      createMenu("cheese", json.menu.cheese[i].name);
     }
     for(var i = 0; i < json.menu.sauces.length; i++){
       createMenu("sauce", json.menu.sauces[i].name);
@@ -42,7 +42,10 @@ window.addEventListener('load', function(event) {
   }
   
   //Set up the menu
-  $("#accordion").accordion({ active: 0 });
+  $("#accordion").accordion({ 
+    active: 0 ,
+    heightStyle: "content"
+  });
   $(".quantity").spinner({
     min : 1,
     max : 100,
@@ -51,8 +54,38 @@ window.addEventListener('load', function(event) {
 });
 
 var createMenu = function (type, name){
+  var elm = document.createElement("div");
   var menuItem = $("<div class=\"" + type + " ingredient\"></div>");
-  menuItem.append("<img src=\"resources/img/" + name + ".png\" alt=\"" + name + "\" class=\"ingredient\">");
+  menuItem.append("<img src=\"resources/img/" + name + ".png\" alt=\"" + name + "\" class=\"ingredient " + type + "\">");
   menuItem.append("<span class=\"caption\">" + name + "</span>");
+  menuItem.click(function(event){
+      var $this = $(this);
+      
+      //Deselect if selected
+      if ($($this.children("img")[0]).hasClass("selected")){
+	$($this.children("img")[0]).removeClass("selected");
+      }
+      else 
+      {
+	if ($(".selected." + type)[0] !== undefined){
+	  $(".selected." + type).removeClass("selected");
+	}
+	$($this.children("img")[0]).addClass("selected");
+      }
+  });
   $("#" + type + "Menu").append(menuItem);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
