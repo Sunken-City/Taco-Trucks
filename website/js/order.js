7eb492b2fefe714d86db56c5ae7c2dbabf601498
picoriley <acloudy@mail.smu.edu>
2014-03-03 16:00:17 -0600

    Getting frustrated with the design. I'm going to commit and focus on functionality for now.

--- website/js/order.js
+++ website/js/order.js
@@ -1,5 +1,6 @@
   
 var json;
+
 window.addEventListener('load', function(event) {
 
   var url = "../taco_truck_menu.json";
@@ -13,28 +14,28 @@ window.addEventListener('load', function(event) {
     json = JSON.parse(request.responseText);
     
     for(var i = 0; i < json.menu.tortillas.length; i++){
-      createMenu("tortilla", json.menu.tortillas[i].name);
+      createMenu("tortilla", json.menu.tortillas[i].name, json.menu.tortillas[i].price);
     }
     for(var i = 0; i < json.menu.type.length; i++){
-      createMenu("filling", json.menu.type[i].name);
+      createMenu("filling", json.menu.type[i].name, json.menu.type[i].price);
     }
     for(var i = 0; i < json.menu.rice.length; i++){
-      createMenu("rice", json.menu.rice[i].name);
+      createMenu("rice", json.menu.rice[i].name, json.menu.rice[i].price);
     }
     for(var i = 0; i < json.menu.beans.length; i++){
-      createMenu("bean", json.menu.beans[i].name);
+      createMenu("bean", json.menu.beans[i].name, json.menu.beans[i].price);
     }
     for(var i = 0; i < json.menu.cheese.length; i++){
-      createMenu("cheese", json.menu.cheese[i].name);
+      createMenu("cheese", json.menu.cheese[i].name, json.menu.cheese[i].price);
     }
     for(var i = 0; i < json.menu.sauces.length; i++){
-      createMenu("sauce", json.menu.sauces[i].name);
+      createMenu("sauce", json.menu.sauces[i].name, json.menu.sauces[i].price);
     }
     for(var i = 0; i < json.menu.vegetables.length; i++){
-      createMenu("veggie", json.menu.vegetables[i].name);
+      createMenu("veggie", json.menu.vegetables[i].name, json.menu.vegetables[i].price);
     }
-    for(var i = 0; i < json.menu.extras.length; i++){
-      createMenu("extras", json.menu.extras[i].name);
+    for(var i = json.menu.extras.length - 1; i > -1; i--){
+      createMenu("extras", json.menu.extras[i].name, json.menu.extras[i].price);
     }
   }
   else {
@@ -53,11 +54,12 @@ window.addEventListener('load', function(event) {
   });
 });
 
-var createMenu = function (type, name){
+var createMenu = function (type, name, price){
   var elm = document.createElement("div");
   var menuItem = $("<div class=\"" + type + " ingredient\"></div>");
   menuItem.append("<img src=\"resources/img/" + name + ".png\" alt=\"" + name + "\" class=\"ingredient " + type + "\">");
   menuItem.append("<span class=\"caption\">" + name + "</span>");
+  menuItem.append("<span class=\"caption\">$" + price.toFixed(2) + "</span>");
   menuItem.click(function(event){
       var $this = $(this);
       //Deselect if selected
@@ -66,12 +68,19 @@ var createMenu = function (type, name){
       }
       else{
 	if ((type !== "veggie")&&(type !== "extras")){
+	  //Unselect any other selected values if the section is 1 or 0-1.
 	  if ($(".selected." + type)[0] !== undefined){
 	    $(".selected." + type).removeClass("selected");
 	  }
+	  //Move to next section.
+	  $("#accordion").accordion("option", "active", $("#accordion").accordion("option", "active") + 1);
 	}
 	$($this.children("img")[0]).addClass("selected");
+	/*$('html, body').animate({
+	  scrollTop: $("#ui-accordion-accordion-header-" + ($("#accordion").accordion("option", "active") - 1)).offset().top
+      }, 400);*/
       }
+      
   });
   $("#" + type + "Menu").append(menuItem);
 };
