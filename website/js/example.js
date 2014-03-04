82ef83a43e59b2ac8dfc630315b8d5370e7c6eb9
Will Spurgin <wspurgin@smu.edu>
2014-03-04 16:17:46 -0600

    Example ajax post request with help form to JSON function

---
+++ website/js/example.js
@@ -0,0 +1,26 @@
+var rootUrl = 'local.tacotruck.com';
+
+function addWine() {
+    console.log('addWine');
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: rootUrl + '/api/login',
+        dataType: "json",
+        data: loginFormToJSON(),
+        success: function() {
+            alert('Success!');
+        },
+        error: function() {
+            alert('Error :(');
+        }
+    });
+}
+
+// Helper function to serialize all the form fields into a JSON string
+function loginFormToJSON() {
+    return JSON.stringify({
+        "email": $('#email').val(),
+        "password": $('#password').val(),
+        });
+}
