23e78072b3311f1cd1f285079b8f4a7eeef1b1cf
Will Spurgin <wspurgin@smu.edu>
2014-03-06 17:23:00 -0600

    Added some styling, updated js

--- website/js/main.js
+++ website/js/main.js
@@ -1,72 +1,69 @@
 window.addEventListener('load', function(event) {
-	var createATacoButton = $('#createATaco');
-	createATacoButton.click(function(){
-		window.location.replace("order.html");
-	});
+    var createATacoButton = $('#createATaco');
+    createATacoButton.click(function() {
+        window.location.replace("order.html");
+    });
 
-		var createAUser = $('#createAccount');
-		createAUser.submit(function(event){
-            event.preventDefault();
-            var checks = {};
-            checks.creditCardNumber = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
-            if(validateForm($(this), checks)) {
-                $.ajax({
-                    type: 'POST',
-                    contentType: 'application/json',
-                    url: '/api/users',
-                    dataType: "json",
-                    data: signUpFormToJSON(),
-                    success: function(data) {
-                        if (data.success) {
-                            window.location.replace("order.html");
-                        }
-                    },
-                    error: function(data) {
-                        alert('Errors occured during your request :(');
-                    }
-                });
-            }
-		});
-
-		var loginAUser = $('#signIn');
-        loginAUser.click(function(event){
-            event.preventDefault();
-			$.ajax({
+    var createAUser = $('#createAccount');
+    createAUser.submit(function(event) {
+        event.preventDefault();
+        var checks = {};
+        checks.creditCardNumber = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
+        if (validateForm($(this), checks)) {
+            $.ajax({
                 type: 'POST',
                 contentType: 'application/json',
-                url: '/api/login',
+                url: '/api/users',
                 dataType: "json",
-                data: loginFormToJSON(),
+                data: signUpFormToJSON(),
                 success: function(data) {
                     if (data.success) {
                         window.location.replace("order.html");
                     }
-                    else {
-                        if(data.message !== undefined) {
-                            alert("We don't have that email! Try creating "+
-                                "an Account!");
-                            autoFillCreateAccount();
-                        }
-                        else {
-                            alert("Login failed");
-                            $('#loginPassword').val('');
-                        }
-                    }
                 },
                 error: function(data) {
-                    alert('Errors occured during your request :(');
-                    $('#loginPassword').val('');
+                    alert('Errors occured during your request.');
                 }
             });
+        }
+    });
+
+    var loginAUser = $('#signIn');
+    loginAUser.click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/api/login',
+            dataType: "json",
+            data: loginFormToJSON(),
+            success: function(data) {
+                if (data.success) {
+                    window.location.replace("order.html");
+                } else {
+                    if (data.message !== undefined) {
+                        alert("We don't have that email! Try creating " +
+                            "an Account!");
+                        autoFillCreateAccount();
+                    } else {
+                        alert("Login failed");
+                        $('#loginPassword').val('');
+                    }
+                }
+            },
+            error: function(data) {
+                alert('Errors occured during your request :(');
+                $('#loginPassword').val('');
+            }
         });
+    });
 
 });
 
 
 
 
-function autoFillCreateAccount()
-{
+function autoFillCreateAccount() {
     var loginEmail = $('#loginEmail');
     var loginPassword = $('#loginPassword');
     var email = loginEmail.val();
@@ -85,23 +82,22 @@ function loginFormToJSON() {
     return JSON.stringify({
         "email": $('#loginEmail').val(),
         "password": $('#loginPassword').val()
-        });
+    });
 }
 // Helper function to serialize all the form fields into a JSON string
 function signUpFormToJSON() {
     return JSON.stringify({
         "email": $('#createEmail').val(),
         "password": $('#createPassword').val(),
-        "f_name":$('#firstName').val(),
-        "l_name":$('#lastName').val(),
-        "cc_p":$('#CardProvider').val(),
-        "cc_n":$('#creditCardNumber').val(),
+        "f_name": $('#firstName').val(),
+        "l_name": $('#lastName').val(),
+        "cc_p": $('#CardProvider').val(),
+        "cc_n": $('#creditCardNumber').val(),
         "tele_num": '000-000-0000'
-        });
+    });
 }
 
-function validateForm(form, checks)
-{
+function validateForm(form, checks) {
     for (var key in checks) {
         console.log(key);
         /* 
@@ -111,9 +107,9 @@ function validateForm(form, checks)
         |____li
         Form structure
         */
-        var element = form.children().children().children("#"+key);
+        var element = form.children().children().children("#" + key);
         //check if the element matches the pattern
-        if(!checks[key].test(element.val())) {
+        if (!checks[key].test(element.val())) {
             return false;
         }
     }
