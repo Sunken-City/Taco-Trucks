fb6ac0374ca7e10aa137a6c8216efe4cbeb02ad1
Will Spurgin <wspurgin@smu.edu>
2014-03-05 22:28:11 -0600

    Added a javascript function to check validity, added back the HTML5 invalidators

--- website/js/main.js
+++ website/js/main.js
@@ -4,29 +4,33 @@ window.addEventListener('load', function(event) {
 		window.location.replace("order.html");
 	});
 
-		var createAUser = $('#signUp');
-		createAUser.click(function(){
-			event.preventDefault();
-			$.ajax({
-                type: 'POST',
-                contentType: 'application/json',
-                url: '/api/users',
-                dataType: "json",
-                data: signUpFormToJSON(),
-                success: function(data) {
-                    if (data.success) {
-                        window.location.replace("order.html");
+		var createAUser = $('#createAccount');
+		createAUser.submit(function(event){
+            event.preventDefault();
+            var checks = {};
+            checks.creditCardNumber = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})/;
+            if(validateForm($(this), checks)) {
+                $.ajax({
+                    type: 'POST',
+                    contentType: 'application/json',
+                    url: '/api/users',
+                    dataType: "json",
+                    data: signUpFormToJSON(),
+                    success: function(data) {
+                        if (data.success) {
+                            window.location.replace("order.html");
+                        }
+                    },
+                    error: function(data) {
+                        alert('Errors occured during your request :(');
                     }
-                },
-                error: function(data) {
-                    alert('Errors occured during your request :(');
-                }
-            });
+                });
+            }
 		});
 
 		var loginAUser = $('#signIn');
-        loginAUser.click(function(){
-
+        loginAUser.click(function(event){
+            event.preventDefault();
 			$.ajax({
                 type: 'POST',
                 contentType: 'application/json',
@@ -54,7 +58,7 @@ window.addEventListener('load', function(event) {
                     $('#loginPassword').val('');
                 }
             });
-		});
+        });
 
 });
 
@@ -94,4 +98,24 @@ function signUpFormToJSON() {
         "cc_n":$('#creditCardNumber').val(),
         "tele_num": '000-000-0000'
         });
+}
+
+function validateForm(form, checks)
+{
+    for (var key in checks) {
+        console.log(key);
+        /* 
+        assumes 
+        form
+        |__ul
+        |____li
+        Form structure
+        */
+        var element = form.children().children().children("#"+key);
+        //check if the element matches the pattern
+        if(!checks[key].test(element.val())) {
+            return false;
+        }
+    }
+    return true;
 }
